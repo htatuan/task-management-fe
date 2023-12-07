3b249ccd4ac2e09dd63eee0517a8045dca12435e
Bao Nghiem <bao.nghiem@clv.local>
2023-12-07 14:29:14 +0700

    register form

--- app/(public)/register/page.tsx
+++ app/(public)/register/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import RegisterForm from './form'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div
+      className="h-screen flex flex-col  
+                    items-center justify-center"
+    >
+      <p className="text-green-700 text-xl mb-3">Register New Account</p>
+      <div className="w-full max-w-xs">
+        <RegisterForm />
+      </div>
+    </div>
   )
 }
 
