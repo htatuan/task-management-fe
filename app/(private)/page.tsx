0f618be7f003c8e909836cf0d995132a57258cc6
Bao Nghiem <bao.nghiem@clv.local>
2023-12-11 15:23:10 +0700

    apply authentication by next auth

--- app/(private)/page.tsx
+++ app/(private)/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 
-const Homepage = async() => {
-  const session = await getServerSession(authOptions);
-  console.log("sesss=> ", session)
-  return <div>Homepage</div>;
+const Homepage = async () => {
+  return (
+    <div>
+      Homepage
+    </div>
+  );
 };
 
 export default Homepage;
--- app/services/useGraphQL.ts
+++ app/services/useGraphQL.ts
@@ -2,12 +2,13 @@ import { GraphQLClient } from "graphql-request";
 
 const API_URL = `http://localhost:3000/graphql`;
 
-const headers = {}
+const headers = {};
+
 const graphQLClient = new GraphQLClient(API_URL, {
   //   headers: {
   //     Authorization: `Bearer ${process.env.API_KEY}`
   //   }
-  headers
+  headers,
 });
 
 export default graphQLClient;
