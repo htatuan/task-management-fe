581629c8d2f9b63914d934c979532b92b6eaae6b
Bao Nghiem <bao.nghiem@clv.local>
2023-12-13 09:51:10 +0700

    remove code test session

--- app/services/useGraphQL.ts
+++ app/services/useGraphQL.ts
@@ -1,14 +1,9 @@
-"use client"
 import { GraphQLClient } from "graphql-request";
-import { useSession } from "next-auth/react"
 
 const API_URL = `http://localhost:3000/graphql`;
 
 const headers = {};
 const callApi = () => {
-  console.log("ASDfasdfasdfasdfasdfasdf")
-  // const { data: session, status } = useSession()
-  // console.log("session=> ", session)
   const graphQLClient = new GraphQLClient(API_URL, {
     headers: {
       Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImJhb25naGllbSIsInN1YiI6MSwiaWF0IjoxNzAyMzY2NTUzLCJleHAiOjE3MDI0MjY1NTN9.pbRCShnvmdPWXPe69lS5HZ0Iq-xtQfbgMSwUC5TVJYo`,
